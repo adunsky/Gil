4c415549feb6a22693314cf2cd2e9bb9684fdccc
amosgery <amos_gerry@hotmail.com>
2015-08-23 12:53:25 +0300

    remove shop db

--- cronjob.sh
+++ cronjob.sh
@@ -4,4 +4,3 @@ chmod 777 /tmp/Google_Client/
 cd /var/www/html/Gilamos
 php calendarServ.php db=samgal > cal.log &
 php calendarServ.php db=demo > calDemo.log & 
-php calendarServ.php db=shop > calShop.log & 
