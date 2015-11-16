3d8194bbcf0ee518bbfc55e62da020a6a6c21113
amosgery <amos_gerry@hotmail.com>
2015-11-16 22:05:54 +0200

    fix bg

--- cronjob.sh
+++ cronjob.sh
@@ -2,8 +2,8 @@
 mkdir /tmp/Google_Client
 chmod 777 /tmp/Google_Client/
 cd /var/www/html/Gilamos
-php calendarServ.php db=samgal
-php calendarServ.php db=demo
-php calendarServ.php db=instdel
-php calendarServ.php db=arava
-php backup.php db=samgal time=00:00
+php calendarServ.php db=samgal &
+php calendarServ.php db=demo & 
+php calendarServ.php db=instdel & 
+php calendarServ.php db=arava & 
+php backup.php db=samgal time=00:00 &
